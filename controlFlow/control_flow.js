42712f5771fa32b9b9882fc91750eef570d66c1f
mariodev01 <mariovalentinomata@gmail.com>
2025-12-30 12:52:50 -0400

    Add ternat operator

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -50,3 +50,11 @@ switch(userType){
 
 
 console.log("User Category:", userCategory);
+
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
+
+
+console.log("Authentication Status:", authenticationStatus);
