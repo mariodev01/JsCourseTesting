a37258ded0ca2621fb1eff8a59943f6b70d05201
mariodev01 <mariovalentinomata@gmail.com>
2025-12-30 11:11:39 -0400

    idk

---
+++ Scope/scope_lab_exercise.js
@@ -0,0 +1,27 @@
+{
+
+    var var1 = "Llamame";
+    let var2 = "Rockstar";
+    const var3 = "K Jenner";
+
+    var1 = "Hello";
+    var2 = "Y ete lio";
+
+    //var3 = "adio";
+    console.log(var1);
+    console.log(var2);
+    console.log(var3);
+
+}
+
+var1 = "Hola";
+
+var2 = "Mundo";
+
+var3 = "Digital 15";
+
+console.log(var1);
+
+console.log(var2);
+
+//console.log(var3);
