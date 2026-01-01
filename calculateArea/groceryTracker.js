361a7baae9f13827a8c24ae78f5261413db40ff4
mariodev01 <mariovalentinomata@gmail.com>
2026-01-01 18:27:02 -0400

    practice task

---
+++ calculateArea/groceryTracker.js
@@ -0,0 +1,15 @@
+let g1;
+let g2;
+let g3;
+
+
+function calculateGroceries(){
+
+    g1 = parseFloat(document.getElementById("g1").value);
+    g2 = parseFloat(document.getElementById("g2").value);
+    g3 = parseFloat(document.getElementById("g3").value);
+
+    let total = g1 + g2 + g3;
+
+    document.getElementById("result").innerText = `The total amount is: $${total.toFixed(2)}`;
+}
