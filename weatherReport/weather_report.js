788fb579a0612a13d2c16237b9421a94b56b48df
mariodev01 <mariovalentinomata@gmail.com>
2026-01-10 20:12:28 -0400

    get weather info by using API

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,27 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+
+    const city = document.getElementById('city').value;
+
+    //console.log(city);
+    const apiKey = 'YOUR_API_KEY'; // Reemplaza 'YOUR_API_KEY' con tu clave API real
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                <p>Temperature: ${data.main.temp} &#8451;</p>
+                                <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Falló al obtener el clima. Por favor, inténtalo de nuevo.</p>`;
+    });
+    
+};
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
