a269b944f4451367f3bc2fb800ca65e825e0f0e4
mariodev01 <mariovalentinomata@gmail.com>
2026-01-15 21:41:58 -0400

    i'm done i think

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,42 +1,64 @@
 const cardContainer = document.getElementById('cardContainer');
 
-function showData(){
+function showData() {
     const input = document.getElementById('txtValue').value.toLowerCase();
 
     cardContainer.innerHTML = '';
 
     fetch('travel_recommendation_api.json')
-    .then(response => response.json())
-    .then(data =>{
-        const d = data.countries.find(item=> item.name.toLowerCase() === input)
-        
+        .then(response => response.json())
+        .then(data => {
+            const d = data.countries.find(item => item.name.toLowerCase() === input)
 
-        if(d){
-            d.cities.forEach((c)=>{
-                console.log(c.imageUrl);
-
-                const templates = `<div class="card" style="width: 18rem;">
-                            <img src="${c.imageUrl}" class="card-img-top" alt="...">
+            if (input === "beach" || input === "Beach" || input === "Beaches" || input === "beaches" || input === "Beache") {
+                data.beaches.forEach((b) => {
+                    const templates = `<div class="card" style="width: 18rem;">
+                            <img src="${b.imageUrl}" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">${c.name}</h5>
-                                <p class="card-text">${c.description}</p>
+                                <h5 class="card-title">${b.name}</h5>
+                                <p class="card-text">${b.description}</p>
                                 <a href="#" class="btn btn-primary">Visit</a>
                             </div>
                         </div>`
                     cardContainer.innerHTML += templates
-            })
-        }else{
-            console.log("Not found");
-        }
-    })
+                });
+            } else if (input === "countries" || input === "country" || input === "Country" || input === "Countries") {
+                data.countries.forEach((c) => {
+                    c.cities.forEach((ci) => {
+                        const templates = `<div class="card" style="width: 18rem;">
+                            <img src="${ci.imageUrl}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${ci.name}</h5>
+                                <p class="card-text">${ci.description}</p>
+                                <a href="#" class="btn btn-primary">Visit</a>
+                            </div>
+                        </div>`
+                        cardContainer.innerHTML += templates
+                    })
+                })
+            } else if (input === "temples" || input === "temple" || input === "Temple" || input === "Temples") {
+                data.temples.forEach((t) => {
+                    const templates = `<div class="card" style="width: 18rem;">
+                            <img src="${t.imageUrl}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${t.name}</h5>
+                                <p class="card-text">${t.description}</p>
+                                <a href="#" class="btn btn-primary">Visit</a>
+                            </div>
+                        </div>`
+                    cardContainer.innerHTML += templates
+                });
+            } else {
+                console.log("Not found");
+            }
+        })
 };
 
 
-function clear(){
+function clear() {
     cardContainer.innerHTML = '';
-    //console.log("Hola");
 }
 
-document.getElementById('searchButton').addEventListener('click',showData);
+document.getElementById('searchButton').addEventListener('click', showData);
 
-document.getElementById('clearButton').addEventListener('click',clear);
+document.getElementById('clearButton').addEventListener('click', clear);
